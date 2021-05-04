9aeefa579742917a74f1585d60ed237262d303fc
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2021-05-03 21:10:53 -0300

    Adicione migrate para cupons

--- database/migrations/1620039303092_category_schema.js
+++ database/migrations/1620039303092_category_schema.js
@@ -4,40 +4,23 @@
 const Schema = use('Schema')
 
 class CategorySchema extends Schema {
-  up () {
-    this.create('categories', (table) => {
+  up() {
+    this.create('categories', table => {
       table.increments()
       table.string('title', 100)
       table.string('description', 255)
       table.integer('image_id').unsigned()
       table.timestamps()
 
-      table.foreign('image_id')
+      table
+        .foreign('image_id')
         .references('id')
         .inTable('images')
-        .onDelete('cascade')
-    })
-
-    this.create('image_product', table => {
-      table.increments()
-      table.integer('image_id').unsigned()
-      table.integer('product_id').unsigned()
-      table.timestamps()
-
-      table.foreign('image_id')
-        .references('id')
-        .inTable('images')
-        .onDelete('cascade')
-
-        table.foreign('product_id')
-        .references('id')
-        .inTable('products')
         .onDelete('cascade')
     })
   }
 
-  down () {
-    this.drop('image_product')
+  down() {
     this.drop('categories')
   }
 }
