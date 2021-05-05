8c9f2b3ce78115474fda6ed8cb53b48e9f87243d
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2021-05-05 19:16:27 -0300

    fix: Ajuste nome Promisse para Promise

--- database/seeds/0002_ClientSeeder.js
+++ database/seeds/0002_ClientSeeder.js
@@ -19,7 +19,7 @@ class ClientSeeder {
   async run() {
     const role = await Role.findBy('slug', 'client')
     const clients = await Factory.model('App/Models/User').createMany(30)
-    await Promisse.all(
+    await Promise.all(
       clients.map(async client => {
         await client.roles().attach([role.id])
       })
@@ -33,7 +33,7 @@ class ClientSeeder {
     })
     const adminRole = await Role.findBy('slug', 'admin')
     await user.roles().attach([adminRole.id])
-
+  }
 }
 
 module.exports = ClientSeeder
