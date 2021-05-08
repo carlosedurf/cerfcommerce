630e44f2d310f13be265a3609aacec99e844b19b
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2021-05-08 13:40:03 -0300

    Adicione Controller de autenticação

--- start/routes/index.js
+++ start/routes/index.js
@@ -16,7 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
+Route.post('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
 
