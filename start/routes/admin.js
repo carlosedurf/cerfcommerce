2b4d9e64b5e12e630ee123cd59efd009fecaafc7
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2021-05-14 22:04:15 -0300

    Adicione lógica de Desconto e cupons

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -22,6 +22,8 @@ Route.group(() => {
   /**
    * Order resoures routes
    */
+  Route.post('orders/:id/discount', 'OrderController.applyDiscount')
+  Route.delete('orders/:id/discount', 'OrderController.removeDiscount')
   Route.resource('orders', 'OrderController').apiOnly()
 
   /**
