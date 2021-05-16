cd8c51a990c3e0582511c06a47cb9260d52db68b
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2021-05-16 17:30:28 -0300

    Adicione Transformer nas categorias

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -5,6 +5,7 @@
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
 const Category = use('App/Models/Category')
+const Transformer = use('App/Transformers/Admin/CategoryTransformer')
 
 /**
  * Resourceful controller for interacting with categories
@@ -17,10 +18,10 @@ class CategoryController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
+   * @param {TransformWith} ctx.transform
    * @param {Object} ctx.pagination
    */
-  async index({ request, response, view, pagination }) {
+  async index({ request, response, transform, pagination }) {
     const title = request.input('title')
 
     const query = Category.query()
@@ -29,7 +30,8 @@ class CategoryController {
       query.where('title', 'LIKE', `%${title}%`)
     }
 
-    const categories = await query.paginate(pagination.page, pagination.limit)
+    var categories = await query.paginate(pagination.page, pagination.limit)
+    categories = await transform.paginate(categories, Transformer)
 
     return response.send(categories)
   }
